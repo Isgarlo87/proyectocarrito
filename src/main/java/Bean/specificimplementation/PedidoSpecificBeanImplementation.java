152f019c097fc34584215c739e738e0519f2d728
a029210979j <a029210979j@S09-PC16>
2017-11-10 09:41:46 +0100

    added fill attr

--- src/main/java/Bean/specificimplementation/PedidoSpecificBeanImplementation.java
+++ src/main/java/Bean/specificimplementation/PedidoSpecificBeanImplementation.java
@@ -20,7 +20,7 @@ import java.util.Date;
  * @author Kysuke
  */
 public class PedidoSpecificBeanImplementation extends TableGenericBeanImplementation {
-
+    
     @Expose
     private Date fecha;
     @Expose
@@ -31,68 +31,68 @@ public class PedidoSpecificBeanImplementation extends TableGenericBeanImplementa
     private Integer id_usuario = 0;
     @Expose(deserialize = false)
     private UsuarioSpecificBeanImplementation obj_usuario = null;
-
+    
     public PedidoSpecificBeanImplementation() {
-
+        
     }
-
+    
     public PedidoSpecificBeanImplementation(Integer id) {
         this.id = id;
     }
-
+    
     public PedidoSpecificBeanImplementation(Integer id_usuario, Date dateFecha) {
         this.id_usuario = id_usuario;
         this.fecha = dateFecha;
     }
-
+    
     public Integer getId() {
         return id;
     }
-
+    
     public void setId(Integer id) {
         this.id = id;
     }
-
+    
     public Date getFecha() {
         return fecha;
     }
-
+    
     public void setFecha(Date fecha) {
         this.fecha = fecha;
     }
-
+    
     public Integer getIva() {
         return iva;
     }
-
+    
     public void setIva(Integer iva) {
         this.iva = iva;
     }
-
+    
     public Boolean getTiene_iva() {
         return tiene_iva;
     }
-
+    
     public void setTiene_iva(Boolean tiene_iva) {
         this.tiene_iva = tiene_iva;
     }
-
+    
     public Integer getId_usuario() {
         return id_usuario;
     }
-
+    
     public void setId_usuario(Integer id_usuario) {
         this.id_usuario = id_usuario;
     }
-
+    
     public UsuarioSpecificBeanImplementation getObj_usuario() {
         return obj_usuario;
     }
-
+    
     public void setObj_usuario(UsuarioSpecificBeanImplementation obj_usuario) {
         this.obj_usuario = obj_usuario;
     }
-
+    
     @Override
     public String getColumns() {
         String strColumns = "";
@@ -102,7 +102,7 @@ public class PedidoSpecificBeanImplementation extends TableGenericBeanImplementa
         strColumns += "id_usuario";
         return strColumns;
     }
-
+    
     @Override
     public String getValues() {
         String strColumns = "";
@@ -112,7 +112,7 @@ public class PedidoSpecificBeanImplementation extends TableGenericBeanImplementa
         strColumns += id_usuario;
         return strColumns;
     }
-
+    
     @Override
     public String toPairs() {
         String strPairs = "";
@@ -121,12 +121,13 @@ public class PedidoSpecificBeanImplementation extends TableGenericBeanImplementa
         strPairs += "id_usuario=" + id_usuario;
         return strPairs;
     }
-
+    
     @Override
     public GenericBeanInterface fill(ResultSet oResultSet, Connection oConnection, UsuarioSpecificBeanImplementation oPuserBean_security, Integer expand) throws SQLException, Exception {
         this.setId(oResultSet.getInt("id"));
         this.setFecha(oResultSet.getDate("fecha"));
         this.setIva(oResultSet.getInt("iva"));
+        this.setTiene_iva(oResultSet.getBoolean("tiene_iva"));
         this.setId_usuario(oResultSet.getInt("id_usuario"));
         if (expand > 0) {
             UsuarioSpecificBeanImplementation oUsuarioBean = new UsuarioSpecificBeanImplementation();
@@ -136,7 +137,7 @@ public class PedidoSpecificBeanImplementation extends TableGenericBeanImplementa
         } else {
             this.setId_usuario(oResultSet.getInt("id_usuario"));
         }
-
+        
         return this;
     }
 }
--- src/main/java/Bean/specificimplementation/UsuarioSpecificBeanImplementation.java
+++ src/main/java/Bean/specificimplementation/UsuarioSpecificBeanImplementation.java
@@ -13,6 +13,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import Bean.publicinterface.GenericBeanInterface;
+import java.util.Date;
+
 /**
  *
  * @author Kysuke
@@ -33,7 +35,8 @@ public class UsuarioSpecificBeanImplementation extends TableGenericBeanImplement
     private String pass;
     @Expose
     private String email;
-
+    @Expose
+    private Date fecha_nacimiento;
     @Expose(serialize = false)
     private Integer id_tipousuario = 0;
     @Expose(deserialize = false)
@@ -119,6 +122,14 @@ public class UsuarioSpecificBeanImplementation extends TableGenericBeanImplement
         this.pass = pass;
     }
 
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
     public TipousuarioSpecificBeanImplementation getObj_tipousuario() {
         return obj_tipousuario;
     }
@@ -175,12 +186,14 @@ public class UsuarioSpecificBeanImplementation extends TableGenericBeanImplement
     @Override
     public GenericBeanInterface fill(ResultSet oResultSet, Connection oConnection, UsuarioSpecificBeanImplementation oPuserBean_security, Integer expand) throws SQLException, Exception {
         this.setId(oResultSet.getInt("id"));
+        this.setDni(oResultSet.getString("dni"));
         this.setNombre(oResultSet.getString("nombre"));
         this.setPrimer_apellido(oResultSet.getString("primer_apellido"));
         this.setSegundo_apellido(oResultSet.getString("segundo_apellido"));
         this.setLogin(oResultSet.getString("login"));
         this.setPass(oResultSet.getString("pass"));
         this.setEmail(oResultSet.getString("email"));
+        this.setFecha_nacimiento(oResultSet.getDate("fecha_nacimieto"));
         this.setId_tipousuario(oResultSet.getInt("id_tipousuario"));
         if (expand > 0) {
             TipousuarioSpecificBeanImplementation oTipousuarioBean = new TipousuarioSpecificBeanImplementation();
