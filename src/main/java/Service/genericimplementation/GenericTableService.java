d39bf9e178fe75fbde60f7b788f7fd37502ee648
Kysuke <Kysuke@192.168.1.66>
2017-11-14 00:19:09 +0100

    Revert "fixed GenericTableService"
    This reverts commit fe68b35bea88fe184ee3c4e441c3e36a927d0b0c.

--- src/main/java/Service/genericimplementation/GenericTableService.java
+++ src/main/java/Service/genericimplementation/GenericTableService.java
@@ -74,7 +74,7 @@ public abstract class GenericTableService extends GenericViewService implements 
     @Override
     public ReplyBean set() throws Exception {
         if (this.checkPermission("set")) {
-            String jason = oRequest.getParameter("json");
+            String jason = oRequest.getParameter("jason");
             Connection oConnection = null;
             ConnectionInterface oPooledConnection = null;
             ReplyBean oReplyBean = null;
